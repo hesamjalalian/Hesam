ff57886d5d1aaffbbbb3934f8d56b7bc66402752
hesamjalalian <72898627+hesamjalalian@users.noreply.github.com>
2021-03-09 18:00:25 -0500

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -9,40 +9,99 @@ from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 import scipy.io as sio
 import os
-from sklearn.model_selection import ShuffleSplit
+import seaborn as sns
 import pandas as pd
 from sklearn.impute import SimpleImputer
 from sklearn import svm
 from joblib import dump, load
+from sklearn.model_selection import ShuffleSplit
+import pickle
+import cv2
+import glob
+from sklearn.model_selection import StratifiedShuffleSplit
 
-dataset = loadmat(r"C:\Users\Hesam\27.2.2021\datasets\Wine.mat")
-data=dataset['dataset']
+file_list = glob.glob('datasets/*.*')
+#print(file_list)
+
+my_list = []   # to store them
+
+path = "datasets/*.*"
+for file in glob.glob(path):
+    #print(file)
+    a = cv2.imread(file)
+    my_list.append(a)
+
+#dataset = loadmat(r"C:\Users\Hesam\27.2.2021\datasets\Banana.mat")
+#data = dataset['dataset']
+
+dataset = loadmat(file)
+data = dataset['dataset']
 #print(dataset)
 
 X = data[:, 0:-1]
 y = data[:, -1]
 
-# I tried here to split once and I am going to update it with 20 repetition
-train_test_split(X, y, test_size=0.2)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+#df = sns.load_dataset('tips')
+#new_data_label = df
+#data = pd.get_dummies(data[:, -1])
 
-#print(X_train)
-#print(X_train)
+#rs = ShuffleSplit(n_splits=20, test_size=0.2, random_state=0)
+#for train_index, test_index,  in rs.split(X):
+#    print("TRAIN:", train_index, "TEST:", test_index)
 
-# normalization
-ms = MinMaxScaler()
-X_train_ms = ms.fit_transform(X_train)
-X_test_ms = ms.transform(X_test)
+#X_train = []
+#X_test = []
+#print(train_index)
+#print(y_train)
 
-#print(X_test_ms)
+#train_test_split(X, y, test_size=0.2)
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# handling the missing data and replace missing values with mean of all the other values
-# I do not apply this function for test_set since it should be unseen data
-imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-imputer = imputer.fit(X_train_ms[:, 1:])
-X_train_ms[:, 1:] = imputer.transform(X_train_ms[:, 1:])
-print(X_train_ms)
+sss = StratifiedShuffleSplit(n_splits=20, test_size=0.2, random_state=0)
+sss.get_n_splits(X, y)
 
-clf = svm.SVC()
-dump(clf, 'X_train_ms.joblib')
-clf = load('X_train_ms.joblib')
+for train_index, test_index in sss.split(X, y):
+    #print("TRAIN:", train_index, "TEST:", test_index)
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+    #print(X_train)
+    #X_train = []
+    #X_test = []
+
+    label_encoder = preprocessing.LabelEncoder()
+    y_train_ms = label_encoder.fit_transform(y_train)
+    y_test_ms = label_encoder.transform(y_test)
+
+    #print(X_train)
+    #print(X_test)
+    # normalization
+    ms = MinMaxScaler()
+    X_train_ms = ms.fit_transform(X_train)
+    X_test_ms = ms.transform(X_test)
+    #print(X_train_ms)
+    #print(X_test_ms)
+
+    # handling the missing data and replace missing values with mean of all the other values
+    imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+    imputer = imputer.fit(X_train_ms[:, 1:])
+    X_train_ms[:, 1:] = imputer.transform(X_train_ms[:, 1:])
+    X_test_ms[:, 1:] = imputer.transform(X_test_ms[:, 1:])
+    #print(X_train_ms)
+    #print(X_test_ms)
+    #print(y_test_ms)
+    #print(y_train)
+
+    X_train_processed = X_train_ms
+    X_test_processed = X_test_ms
+    y_train_processed = y_train_ms
+    y_test_processed = y_test_ms
+
+    processed = ['X_tran_processed, X_test_processed, y_train_processed, y_test_processed']
+    with open ('processed.pkl', 'wb') as processedpickle:
+        pickle.dump(processed, processedpickle)
+    print(X_train_processed)
+    #print(y_test_processed)
+
+#processed = ['X_train_ms, X_test_ms, y_train, y_test']
+#with open ('processed.pkl', 'wb') as processedpickle:
+#    pickle.dump(processed, processedpickle)
