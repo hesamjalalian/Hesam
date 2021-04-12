ffab09f77a5b3d588c494569fb926fef094fa5f9
unknown <hesam.jalalian@yahoo.com>
2021-04-12 14:28:52 -0400

    random forest was added. also boosting with perceptron works for 20 datasets

--- Bagging_DecisionTree_Perceptron.py
+++ Bagging_DecisionTree_Perceptron.py
@@ -1,4 +1,8 @@
 import glob
+import deslib
+from deslib.static import Oracle
+from sklearn.utils.validation import check_X_y, check_array
+import self
 from numpy import mean
 import numpy as np
 from sklearn.ensemble import BaggingClassifier
@@ -11,6 +15,25 @@ from sklearn.linear_model import Perceptron
 from tabulate import tabulate
 from dataclasses import make_dataclass
 from sklearn.ensemble import RandomForestClassifier
+from deslib.static.base import BaseStaticEnsemble
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_classification
+
+def random_forest(x_train, y_train, model_name):
+    random_forest_model = RandomForestClassifier(random_state=42)
+
+    split_num = model_name.split('_')[4].split('.')[0]
+    portion = model_name.split('\\')[1]
+    model = random_forest
+    pkl_filename = "model_files/{}/split_{}/random_forest.pkl".format(portion, split_num)
+    with open(pkl_filename, 'wb') as file:
+        pickle.dump(model, file)
+
+    random_forest_model.fit(x_train, y_train)
+    y_pred = random_forest_model.predict(x_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    # print('Accuracy of Bagging-decision tree base classifier: {:.3f}'.format(accuracy)) # rondesh karde
+    return accuracy
 
 def bagging_with_decision_tree(x_train, y_train, model_name):
     dt = DecisionTreeClassifier(random_state=42)
@@ -63,45 +86,19 @@ def boosting_with_decision_tree(x_train, y_train, model_name):
     # print('Accuracy of Bagging-perceptron base Classifier: {:.3f}'.format(accuracy)) # rondesh karde
     return accuracy
 
-def Boosting_with_perceptron(x_train, y_train):
+def Boosting_with_perceptron(x_train, y_train, model_name):
     bperc = Perceptron(tol=1e-3, random_state=42)
-    Boosting_classifiers_Perceptron_AsBaseEstimator = AdaBoostClassifier(base_estimator=bperc, n_estimators=100, random_state=0, algorithm='SAMME')
-    Boosting_classifiers_Perceptron_AsBaseEstimator.fit(x_train, y_train)
-    y_pred = Boosting_classifiers_Perceptron_AsBaseEstimator.predict(x_test)
-    accuracy = accuracy_score(y_test, y_pred)  # Evaluate the accuracy
-    # print('Accuracy of Bagging-perceptron base Classifier: {:.3f}'.format(accuracy)) # rondesh karde
-    return accuracy
-
-def bagging_with_random_forest(x_train, y_train, model_name):
-    rf = RandomForestClassifier(random_state=42)
-    Bagging_classifiers_randomforest_AsBaseEstimator = BaggingClassifier(base_estimator=rf, n_estimators=100, n_jobs=-1, random_state=42)
+    Boosting_classifiers_perceptron_AsBaseEstimator = AdaBoostClassifier(base_estimator=bperc, n_estimators=100, random_state=0, algorithm='SAMME')
 
     split_num = model_name.split('_')[4].split('.')[0]
     portion = model_name.split('\\')[1]
-    model = Bagging_classifiers_randomforest_AsBaseEstimator
-    pkl_filename = "model_files/{}/split_{}/bagging_with_decision_tree.pkl".format(portion, split_num)
+    model = Boosting_classifiers_perceptron_AsBaseEstimator
+    pkl_filename = "model_files/{}/split_{}/boosting_with_perceptron.pkl".format(portion, split_num)
     with open(pkl_filename, 'wb') as file:
         pickle.dump(model, file)
 
-    Bagging_classifiers_randomforest_AsBaseEstimator.fit(x_train, y_train)
-    y_pred = Bagging_classifiers_randomforest_AsBaseEstimator.predict(x_test) # Predict test set labels
-    accuracy = accuracy_score(y_test, y_pred) # Evaluate the accuracy
-    # print('Accuracy of Bagging-decision tree base classifier: {:.3f}'.format(accuracy)) # rondesh karde
-    return accuracy
-
-def boosting_with_random_forest(x_train, y_train, model_name):
-    brf = RandomForestClassifier(random_state=42)
-    boosting_classifiers_randomforest_AsBaseEstimator = AdaBoostClassifier(base_estimator=brf, n_estimators=100, random_state=0, algorithm='SAMME')  # Instantiate a BoostingClassifier 'Boosting_classifiers_Perceptron_AsBaseEstimator'
-
-    split_num = model_name.split('_')[4].split('.')[0]
-    portion = model_name.split('\\')[1]
-    model = boosting_classifiers_randomforest_AsBaseEstimator
-    pkl_filename = "model_files/{}/split_{}/boosting_with_decision_tree.pkl".format(portion, split_num)
-    with open(pkl_filename, 'wb') as file:
-        pickle.dump(model, file)
-
-    boosting_classifiers_randomforest_AsBaseEstimator.fit(x_train, y_train)
-    y_pred = boosting_classifiers_randomforest_AsBaseEstimator.predict(x_test)
+    Boosting_classifiers_perceptron_AsBaseEstimator.fit(x_train, y_train)
+    y_pred = Boosting_classifiers_perceptron_AsBaseEstimator.predict(x_test)
     accuracy = accuracy_score(y_test, y_pred)  # Evaluate the accuracy
     # print('Accuracy of Bagging-perceptron base Classifier: {:.3f}'.format(accuracy)) # rondesh karde
     return accuracy
@@ -110,35 +107,33 @@ def save_obj(obj, name ):
     with open('{}.pkl'.format(name), 'wb') as configFile:
         pickle.dump(obj, configFile)
 
-# dictionary bejaye list
+
 dict_final_results_Bagging_decision_tree = {}
 dict_final_results_Bagging_perceptron = {}
 dict_final_results_Boosting_decision_tree = {}
-dict_final_results_Bagging_random_forest = {}
-dict_final_results_Boosting_random_forest = {}
-# dict_final_results_Boosting_perceptron = {}
+dict_final_results_random_forest = {}
+dict_final_results_Boosting_perceptron = {}
+
 
 dict_standard_deviation_decisionTreeAccutrcies = {}
 dict_standard_deviation_perceptronAccurecies = {}
 dict_standard_deviation_boostingdecisionTreeAccutrcies = {}
-dict_standard_deviation_bagging_randomforestAccutrcies = {}
-dict_standard_deviation_boosting_randomforestAccutrcies = {}
+dict_standard_deviation_randomforestAccutrcies = {}
+dict_standard_deviation_boostingperceptronAccutrcies = {}
 
 for files in glob.glob('preprocessed_files/*'):
     print(files)
     decisionTreeAccutrcies=[]
     perceptronAccurecies =[]
     boostingdecisionTreeAccutrcies = []
-    baggingrandomforestAccutrcies = []
-    boostingrandomforestAccutrcies = []
+    randomforestAccutrcies = []
+    boostingperceptronAccutrcies = []
 
     standard_deviation_decisionTreeAccutrcies = []
     standard_deviation_perceptronAccurecies = []
     standard_deviation_boostingdecisionTreeAccutrcies = []
-    standard_deviation_bagging_randomforestAccutrcies = []
-    standard_deviation_boosting_randomforestAccutrcies =[]
-
-    # BoostingperceptronAccurecies = []
+    standard_deviation_randomforestAccutrcies = []
+    standard_deviation_boostingperceptronAccutrcies = []
 
     for items in glob.glob("{}/*.*".format(files)):
         # print(items)
@@ -160,85 +155,79 @@ for files in glob.glob('preprocessed_files/*'):
         accuracy_bagging_with_decision_tree = bagging_with_decision_tree(x_train, y_train, items)
         accuracy_bagging_with_perceptron = bagging_with_perceptron(x_train, y_train, items)
         accuracy_Boosting_with_decision_tree = boosting_with_decision_tree(x_train, y_train, items)
-        accuracy_bagging_with_random_forest = bagging_with_random_forest(x_train, y_train, items)
-        accuracy_boosting_with_random_forest = boosting_with_random_forest(x_train, y_train, items)
-        # accuracy_Boosting_with_perceptron = Boosting_with_perceptron(x_train, y_train)
+        accuracy_random_forest = random_forest(x_train, y_train, items)
+        accuracy_Boosting_with_perceptron = Boosting_with_perceptron(x_train, y_train, items)
 
-        decisionTreeAccutrcies.append(accuracy_bagging_with_decision_tree)  #add accuracies to list
+        decisionTreeAccutrcies.append(accuracy_bagging_with_decision_tree)
         perceptronAccurecies.append(accuracy_bagging_with_perceptron)
-        boostingdecisionTreeAccutrcies.append(accuracy_Boosting_with_decision_tree) # add accuracies for boosting
-        baggingrandomforestAccutrcies.append(accuracy_bagging_with_random_forest)
-        boostingrandomforestAccutrcies.append(accuracy_boosting_with_random_forest)
-
-        # BoostingperceptronAccurecies.append(accuracy_Boosting_with_perceptron)
+        boostingdecisionTreeAccutrcies.append(accuracy_Boosting_with_decision_tree)
+        randomforestAccutrcies.append(accuracy_random_forest)
+        boostingperceptronAccutrcies.append(accuracy_Boosting_with_perceptron)
 
     dict_final_results_Bagging_decision_tree["{}".format(files)] = mean(decisionTreeAccutrcies)
     dict_final_results_Bagging_perceptron["{}".format(files)] = mean(perceptronAccurecies)
     dict_final_results_Boosting_decision_tree["{}".format(files)] = mean(boostingdecisionTreeAccutrcies)
-    dict_final_results_Bagging_random_forest["{}".format(files)] = mean(baggingrandomforestAccutrcies)
-    dict_final_results_Boosting_random_forest["{}".format(files)] = mean(boostingrandomforestAccutrcies)
-    # dict_final_results_Boosting_perceptron["{}".format(files)] = mean(BoostingperceptronAccurecies)
+    dict_final_results_random_forest["{}".format(files)] = mean(randomforestAccutrcies)
+    dict_final_results_Boosting_perceptron["{}".format(files)] = mean(boostingperceptronAccutrcies)
 
     np.std(decisionTreeAccutrcies, dtype=np.float64)
     np.std(perceptronAccurecies, dtype=np.float64)
     np.std(boostingdecisionTreeAccutrcies, dtype=np.float64)
-    np.std(baggingrandomforestAccutrcies, dtype=np.float64)
-    np.std(boostingrandomforestAccutrcies, dtype=np.float64)
+    np.std(randomforestAccutrcies, dtype=np.float64)
+    np.std(boostingperceptronAccutrcies, dtype=np.float64)
 
     standard_deviation_decisionTreeAccutrcies.append(np.std(decisionTreeAccutrcies, dtype=np.float64))
     standard_deviation_perceptronAccurecies.append(np.std(perceptronAccurecies, dtype=np.float64))
     standard_deviation_boostingdecisionTreeAccutrcies.append(np.std(boostingdecisionTreeAccutrcies, dtype=np.float64))
-    standard_deviation_bagging_randomforestAccutrcies.append(np.std(baggingrandomforestAccutrcies, dtype=np.float64))
-    standard_deviation_boosting_randomforestAccutrcies.append(np.std(boostingrandomforestAccutrcies, dtype=np.float64))
+    standard_deviation_randomforestAccutrcies.append(np.std(randomforestAccutrcies, dtype=np.float64))
+    standard_deviation_boostingperceptronAccutrcies.append(np.std(boostingperceptronAccutrcies, dtype=np.float64))
     # print(standard_deviation_decisionTreeAccutrcies)
 
     dict_standard_deviation_decisionTreeAccutrcies["{}".format(files)] = mean(standard_deviation_decisionTreeAccutrcies)
     dict_standard_deviation_perceptronAccurecies["{}".format(files)] = mean(standard_deviation_perceptronAccurecies)
     dict_standard_deviation_boostingdecisionTreeAccutrcies["{}".format(files)] = mean(standard_deviation_boostingdecisionTreeAccutrcies)
-    dict_standard_deviation_bagging_randomforestAccutrcies["{}".format(files)] = mean(standard_deviation_bagging_randomforestAccutrcies)
-    dict_standard_deviation_boosting_randomforestAccutrcies["{}".format(files)] = mean(standard_deviation_boosting_randomforestAccutrcies)
+    dict_standard_deviation_randomforestAccutrcies["{}".format(files)] = mean(standard_deviation_randomforestAccutrcies)
+    dict_standard_deviation_boostingperceptronAccutrcies["{}".format(files)] = mean(standard_deviation_boostingperceptronAccutrcies)
 
 print(dict_final_results_Bagging_decision_tree)
 print(dict_final_results_Bagging_perceptron)
 print(dict_final_results_Boosting_decision_tree)
-print(dict_final_results_Bagging_random_forest)
-print(dict_final_results_Boosting_random_forest)
-# print(dict_final_results_Boosting_perceptron)
+print(dict_final_results_random_forest)
+print(dict_final_results_Boosting_perceptron)
 
 print(dict_final_results_Bagging_decision_tree.values())
 print(dict_final_results_Bagging_perceptron.values())
 print(dict_final_results_Boosting_decision_tree.values())
-print(dict_final_results_Bagging_random_forest.values())
-print(dict_final_results_Boosting_random_forest.values())
+print(dict_final_results_random_forest.values())
+print(dict_final_results_Boosting_perceptron.values())
 
 print(dict_standard_deviation_decisionTreeAccutrcies.values())
 print(dict_standard_deviation_perceptronAccurecies.values())
 print(dict_standard_deviation_boostingdecisionTreeAccutrcies.values())
-print(dict_standard_deviation_bagging_randomforestAccutrcies.values())
-print(dict_standard_deviation_boosting_randomforestAccutrcies.values())
+print(dict_standard_deviation_randomforestAccutrcies.values())
+print(dict_standard_deviation_boostingperceptronAccutrcies.values())
 
-#save file
+# #save file
 save_obj(dict_final_results_Bagging_decision_tree,"final_results_bagging_decisiontree")
 save_obj(dict_final_results_Bagging_perceptron,"final_results_bagging_perceptron")
 save_obj(dict_final_results_Boosting_decision_tree,"final_results_boosting_decisiontree")
-save_obj(dict_final_results_Bagging_random_forest,"final_results_bagging_randomforest")
-save_obj(dict_final_results_Boosting_decision_tree,"final_results_boosting_randomforest")
-# save_obj(dict_final_results_Boosting_perceptron,"final_results_boosting_decisiontree")
+save_obj(dict_final_results_random_forest,"final_results_randomforest")
+save_obj(dict_final_results_Boosting_perceptron,"final_results_boosting_perceptron")
 
 Bagging_decision_tree = (dict_final_results_Bagging_decision_tree.values())
 Bagging_perceptron = (dict_final_results_Bagging_perceptron.values())
 Boosting_decision_tree = (dict_final_results_Boosting_decision_tree.values())
-Bagging_random_forest = (dict_final_results_Bagging_random_forest.values())
-Boosting_random_forest = (dict_final_results_Boosting_random_forest.values())
+random_forest = (dict_final_results_random_forest.values())
+Boosting_perceptron = (dict_final_results_Boosting_perceptron.values())
 
 standard_deviation_bagging_decisionTree = (dict_standard_deviation_decisionTreeAccutrcies.values())
 standard_deviation_Bagging_perceptron = (dict_standard_deviation_perceptronAccurecies.values())
 standard_deviation_boosting_decisionTree = (dict_standard_deviation_boostingdecisionTreeAccutrcies.values())
-standard_deviation_bagging_randomforest = (dict_standard_deviation_bagging_randomforestAccutrcies.values())
-standard_deviation_boosting_randomforest = (dict_standard_deviation_boosting_randomforestAccutrcies.values())
+standard_deviation_randomforest = (dict_standard_deviation_randomforestAccutrcies.values())
+standard_deviation_boosting_perceptron = (dict_standard_deviation_boostingperceptronAccutrcies.values())
 
-list_of_tuples = list(zip(Bagging_decision_tree,standard_deviation_bagging_decisionTree, Bagging_perceptron,standard_deviation_Bagging_perceptron, Boosting_decision_tree,standard_deviation_boosting_decisionTree ))
-list_of_tuples
+# list_of_tuples = list(zip(Bagging_decision_tree,standard_deviation_bagging_decisionTree, Bagging_perceptron,standard_deviation_Bagging_perceptron, Boosting_decision_tree,standard_deviation_boosting_decisionTree ))
+# list_of_tuples
 # df = pd.DataFrame(list_of_tuples,columns=['Bagging_decision_tree','standard_deviation_bagging_decisionTree', 'Bagging_perceptron', 'standard_deviation_Bagging_perceptron', 'Boosting_decision_tree', 'standard_deviation_boosting_decisionTree'])
 # print(df)
 
@@ -247,21 +236,6 @@ list_of_tuples
 #
 # with open('filename.txt', 'w') as outputfile:
 #     print(tabulate(df), file=outputfile)
-
-Point = make_dataclass("Point", [("Bagging_decision_tree", int),
-                                 ("standard_deviation_bagging_decisionTree", int),
-                                 ("Bagging_perceptron", int),
-                                 ("standard_deviation_Bagging_perceptron", int),
-                                 ("Boosting_decision_tree", int),
-                                 ("standard_deviation_boosting_decisionTree", int)])
-df = pd.DataFrame(list_of_tuples,columns=['Bagging_decision_tree','standard_deviation_bagging_decisionTree', 'Bagging_perceptron', 'standard_deviation_Bagging_perceptron', 'Boosting_decision_tree', 'standard_deviation_boosting_decisionTree'])
-
-with open('table.txt', 'w') as f:
-    f.write(tabulate(df))
-
-with open('filename.txt', 'w') as outputfile:
-    print(tabulate(df), file=outputfile)
-
 
 
 
