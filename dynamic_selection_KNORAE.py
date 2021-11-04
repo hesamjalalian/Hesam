17a3ce935bb1c6f280d381b7e9a97e78c54ea05a
unknown <hesam.jalalian@yahoo.com>
2021-11-03 23:05:25 -0400

    it is updated

--- dynamic_selection_KNORAE.py
+++ dynamic_selection_KNORAE.py
@@ -6,32 +6,46 @@ import numpy as np
 from numpy import mean
 import pickle
 
-knorae_results = []
-accuracy = []
+
+
 dict_final_results = {}
 dict_standard_deviation = {}
 
-for files in glob.glob('preprocessed_files/*'):
+
+for files in glob.glob('processed_3/*'):
     print(files)
+    standard_deviation = []
+    knorae_results = []
+    accuracy = []
+
     for items in glob.glob("{}/*.*".format(files)):
         print(items)
 
-        standard_deviation = []
+
 
         file_name = files.split("\\")[1]
-        print(file_name)
+        # print(file_name)
         item_name = items.split("\\")[2].split(".")[0].split("_")[2]
-        print(item_name)
+        # print(item_name)
+        ss = file_name.split("_")[0]
+        # print(ss)
         model_load = glob.glob("model_files/{}/split_{}/bagging_with_decision_tree.pkl".format(file_name, item_name))[0]
         # model_load = glob.glob("model_files/{}/split_{}/bagging_with_perceptron.pkl".format(file_name, item_name))[0]
         # model_load = glob.glob("model_files/{}/split_{}/boosting_with_decision_tree.pkl".format(file_name, item_name))[0]
         # model_load = glob.glob("model_files/{}/split_{}/boosting_with_perceptron.pkl".format(file_name, item_name))[0]
         # model_load = glob.glob("model_files/{}/split_{}/random_forest.pkl".format(file_name, item_name))[0]
+        # model_load = glob.glob("Models_FLT_10_2/{}/{}_{}.pkl".format(file_name,ss, item_name))[0]
+        # model_load = glob.glob("Models_FLT/{}/{}_{}.pkl".format(file_name, ss, item_name))[0]
+        # model_load = glob.glob("modelsss/{}/{}_{}.pkl".format(file_name, item_name))[0]
+        # model_load = glob.glob("modelsss/{}/{}_{}.pkl".format(file_name, ss, item_name))[0]
 
-        print(model_load)
+
+        # print(model_load)
+        # print(type(model_load))
+
         with open(model_load, 'rb') as file:
             my_model = pickle.load(file)
-            print(type(my_model))
+            # print(type(my_model))
 
             data = np.load(items, allow_pickle=True)
             # print(data)
@@ -40,12 +54,14 @@ for files in glob.glob('preprocessed_files/*'):
             x_test = input_list['FrameStack'][1]
             y_train = input_list['FrameStack'][2]
             y_test = input_list['FrameStack'][3]
+            x_dsel = input_list['FrameStack'][4]
+            y_dsel = input_list['FrameStack'][5]
             # print(x_train)
             # print(len(x_train))
             # print(len(x_test))
 
 
-            x_trainnn, x_dsel, y_trainnn, y_dsel = train_test_split(x_train, y_train, test_size=0.33,random_state=42)
+            # x_trainnn, x_dsel, y_trainnn, y_dsel = train_test_split(x_train, y_train, test_size=0.33,random_state=42)
             # print(len(x_trainnn))
             # print(len(x_dsel))
             # print(len(y_trainnn))
@@ -67,13 +83,24 @@ for files in glob.glob('preprocessed_files/*'):
             print(acc)
             knorae_results.append(acc)
 
+
+
+            # meannn = mean(acc)
+            # print(mean(ola_results))
+
     dict_final_results["{}".format(files)] = mean(knorae_results)
     print(dict_final_results)
-    np.std(knorae_results, dtype=np.float64)
-    standard_deviation.append(np.std(knorae_results, dtype=np.float64))
-    dict_standard_deviation["{}".format(files)] = mean(standard_deviation)
-    print(dict_standard_deviation.values())
 
 
-print(dict_standard_deviation.values())
-standard_deviation = (dict_standard_deviation.values())
+#
+#     dict_final_results["{}".format(files)] = mean(knorae_results)
+#     print(dict_final_results)
+#     np.std(knorae_results, dtype=np.float64)
+#     standard_deviation.append(np.std(knorae_results, dtype=np.float64))
+#     dict_standard_deviation["{}".format(files)] = mean(standard_deviation)
+#     print(dict_standard_deviation.values())
+#
+#
+# print(dict_standard_deviation.values())
+# standard_deviation = (dict_standard_deviation.values())
+
